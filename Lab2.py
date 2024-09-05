63b4f88625f9fa734cf92ef278370d2afa9afea3
tanu1718 <trana01@g.syr.edu>
2024-09-05 14:50:39 +0000

    retreived apikey from secrets

--- Lab2.py
+++ Lab2.py
@@ -11,7 +11,7 @@ st.write(
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+openai_api_key =  st.secrets["api_key"]
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
@@ -32,6 +32,8 @@ else:
             placeholder="Can you give me a short summary?",
             disabled=not uploaded_file,
         )
+
+
 
         if uploaded_file and question:
 
